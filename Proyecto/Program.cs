847652acd18b253455af6c15817912072becf4ea
edwin <edwinmachucachavez@gmail.com>
2025-09-24 15:48:15 -0300

    Actualizado

--- Proyecto/Program.cs
+++ Proyecto/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddSingleton<QRService>();
-
+// Las Interfaz y los repository 
 builder.Services.AddTransient<IClienteRepository, ClienteRepository>();
 builder.Services.AddTransient<IEntradaRepository,EntradaRepository >();
 builder.Services.AddTransient<IEventoRepository, EventoRepository>();
---
+++ Proyecto/Proyecto.TestRepos/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Proyecto.TestRepos;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
